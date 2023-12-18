69e2f1dd621174f126adf44f8ff3bc9da9687cf6
Mano <vmanavalan2517@outlook.in>
2023-12-18 05:44:50 +0530

    Practice Problems Added

---
+++ C++/C++ Practice Problems/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+void Problem_1();
+
+int main() {
+    Problem_1();
+    return 0;
+}
---
+++ C++/C++ Practice Problems/Problem_1.cpp
@@ -0,0 +1,22 @@
+/*
+Problem_1:- 1
+Sum of two numbers
+*/
+
+
+#include <iostream>
+using namespace std;
+
+
+void Problem_1() 
+{
+    int num1;
+    int num2;
+    int sum;
+    cout << "Enter number 1: ";
+    cin >> num1;
+    cout << "Enter number 2: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum of two numbers = " << sum;
+}
